5ade2092b01c3d377138be089f2a607b1771a3ce
Vinicio Shurman <48026024+vinicioshurman@users.noreply.github.com>
2022-05-13 12:17:30 -0300

    type error

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ if (isset($_GET['action'])) {
         require_once 'view/perguntasFrequentes.php'
     }
 
-    if ($_GET[''] == 'contato') {
+    if ($_GET['action'] == 'contato') {
         $title = "Contato";
         require_once 'view/contato.php'
     }
